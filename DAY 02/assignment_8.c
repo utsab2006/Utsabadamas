c44af6a314b879740b9bcccd9aa0c83deebcb127
utsab2006 <utsabmaiti2006@gmail.com>
2025-08-23 21:51:53 +0530

    assignment_8.c

--- DAY 02/assignment_8.c
+++ DAY 02/assignment_8.c
@@ -3,16 +3,14 @@
 int main() {
     float fahrenheit, celsius;
 
-    // Taking input in Fahrenheit
     printf("Enter temperature in Fahrenheit: ");
     scanf("%f", &fahrenheit);
 
-    // Conversion formula
     celsius = (fahrenheit - 32) * 5 / 9;
 
-    // Display result
     printf("Temperature in Celsius: %.2f\n", celsius);
 
     return 0;
 }
+
 
