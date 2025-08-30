3c3679563e48532cda1a26a80fb55f178773b6cc
utsab2006 <utsabmaiti2006@gmail.com>
2025-08-30 20:44:17 +0530

    Assignment 15.c

--- Assignment 15.c
+++ Assignment 15.c
@@ -2,32 +2,32 @@
 #include <math.h>
 
 int main() {
-    float a, b, c, discriminant, root1, root2, realPart, imagPart;
+    float a, b, c, d, root1, root2;
 
     printf("Enter coefficients a, b and c: ");
     scanf("%f %f %f", &a, &b, &c);
 
+    d = b*b - 4*a*c;  
 
-    discriminant = b * b - 4 * a * c;
-    if (discriminant > 0) {
-        root1 = (-b + sqrt(discriminant)) / (2 * a);
-        root2 = (-b - sqrt(discriminant)) / (2 * a);
-        printf("Roots are real and different.\n");
-        printf("Root1 = %.2f and Root2 = %.2f\n", root1, root2);
+    if(d > 0) {
+        root1 = (-b + sqrt(d)) / (2*a);
+        root2 = (-b - sqrt(d)) / (2*a);
+        printf("Roots are real and different\n");
+        printf("Root1 = %.2f\n", root1);
+        printf("Root2 = %.2f\n", root2);
     }
-    else if (discriminant == 0) {
-        root1 = root2 = -b / (2 * a);
-        printf("Roots are real and equal.\n");
+    else if(d == 0) {
+    
+        root1 = root2 = -b / (2*a);
+        printf("Roots are real and equal\n");
         printf("Root1 = Root2 = %.2f\n", root1);
     }
-
     else {
-        realPart = -b / (2 * a);
-        imagPart = sqrt(-discriminant) / (2 * a);
-        printf("Roots are complex and different.\n");
-        printf("Root1 = %.2f + %.2fi\n", realPart, imagPart);
-        printf("Root2 = %.2f - %.2fi\n", realPart, imagPart);
+    
+        printf("Roots are imaginary (complex numbers)\n");
     }
 
     return 0;
 }
+
+
