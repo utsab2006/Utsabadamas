c163ed3431fbfd124fd2f86dc47bfa7bcc234fd2
utsab2006 <utsabmaiti2006@gmail.com>
2025-08-30 20:49:59 +0530

    Assignment 11.c

--- Assignment 11.c
+++ Assignment 11.c
@@ -1,23 +1,13 @@
 #include <stdio.h>
-#include <math.h> 
 
 int main() {
-    double principal, rate, time, amount, compoundInterest;
-    printf("Enter Principal amount: ");
-    scanf("%lf", &principal);
+    int num, lastDigit;
 
-    printf("Enter Rate of Interest (in %%): ");
-    scanf("%lf", &rate);
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+    lastDigit = num - (num / 10) * 10;
 
-    printf("Enter Time (in years): ");
-    scanf("%lf", &time);
-
-    amount = principal * pow((1 + rate / 100), time);
-    compoundInterest = amount - principal;
-
-
-    printf("\nFinal Amount = %.2lf", amount);
-    printf("\nCompound Interest = %.2lf\n", compoundInterest);
+    printf("Last digit of %d is %d\n", num, lastDigit);
 
     return 0;
 }
